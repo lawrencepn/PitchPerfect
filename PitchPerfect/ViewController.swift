8c11b82eccf779277a82dc9a80801b5a0268efae
lawrencenyakiso <lawrencenyakiso@gmail.com>
2016-01-28 10:21:51 +0200

    Initial Commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Lawrence Nyakiso on 2016/01/28.
+//  Copyright © 2016 KisoCloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
